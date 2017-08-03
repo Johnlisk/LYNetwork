adbf3b44bcdceb6ece5b8ea6ea188b50fdc190df
xuhaonan <haonanxu@yangqianguan.com>
2017-08-03 15:44:02 +0800

    MOD: add md5 for file name

--- LYNetwork/Source/LYNetworkPrivate.swift
+++ LYNetwork/Source/LYNetworkPrivate.swift
@@ -22,6 +22,7 @@
 //  THE SOFTWARE.
 
 import Foundation
+import CommonCrypto
 
 // MARK: - Debug Logger
 func lyDebugPrintLog<T>(message: T,
@@ -155,6 +156,23 @@ open class LYNetworkError {
     }
     let nsError: NSError = error as NSError
     return nsError.lyDescription()
+  }
+}
+
+public extension String {
+  public func MD5String() -> String? {
+    let length = Int(CC_MD5_DIGEST_LENGTH)
+    var digest = [UInt8](repeating: 0, count: length)
+    
+    if let d = self.data(using: String.Encoding.utf8) {
+      _ = d.withUnsafeBytes { (body: UnsafePointer<UInt8>) in
+        CC_MD5(body, CC_LONG(d.count), &digest)
+      }
+    }
+    
+    return (0..<length).reduce("") {
+      $0 + String(format: "%02x", digest[$1])
+    }
   }
 }
 
--- LYNetwork/Source/LYRequest.swift
+++ LYNetwork/Source/LYRequest.swift
@@ -368,7 +368,7 @@ public class LYRequest: LYBaseRequest {
     } else {
       requestInfo = String.init(format: "Method:%ld Host:%@ Url:%@ Argument:%@", self.requestMethod().rawValue , baseUrl, requestUrl, argument!)
     }
-    return requestInfo
+    return requestInfo.MD5String()!
     
   }
   
